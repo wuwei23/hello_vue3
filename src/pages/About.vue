a8f8049fc6a79ade491e2d5242f7fe219b7c96de
wuwei23 <2650408922@qq.com>
2025-02-27 09:16:10 +0800

    路由修改

---
+++ src/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about">
+    <h2>大家好，欢迎来到尚硅谷直播间</h2>
+    </div></template>
+<script setup lang="ts" name="About">
+    import {onMounted, onUnmounted} from 'vue'
+
+    onMounted(()=>{
+        console.log('about页面挂载了')
+    })
+    onUnmounted(()=>{
+        console.log('about页面卸载了')
+    })
+</script>
+<style scoped>
+.about{
+    display: flex;
+    justify-content: center;align-items: center;height: 100%;
+    color:rgb(85,84,84);
+    font-size: 18px;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,24 @@
 import { createRouter, createWebHashHistory} from "vue-router";
-import About from "@/components/About.vue";
-import News from "@/components/News.vue";
-import Home from "@/components/Home.vue";
+import About from "@/pages/About.vue";
+import News from "@/pages/News.vue";
+import Home from "@/pages/Home.vue";
 
 //创建路由器
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
+      name: 'news',
       path: "/news",
       component: News,
     },
     {
+      name: 'home',
       path: "/home",
       component: Home,
     },
     {
+      name: 'about',
       path: "/about",
       component: About,
     }
